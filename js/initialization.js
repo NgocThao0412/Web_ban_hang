67c2c73249f0517fffc1f1ae2798ff5ee99b7cf2
Anh031006 <dangngocminhanh76@gmail.com>
2025-10-14 16:37:22 +0000

    thêm

---
+++ js/initialization.js
@@ -0,0 +1,16 @@
+function createProduct() {
+    if (localStorage.getItem('products') == null) {
+        let products = [{
+            id: 1,
+            status:'',
+            title:'',
+            img:'',
+            price: 1,
+            
+        },
+        ]
+    }
+}
+function createAdminAccount() {
+
+}

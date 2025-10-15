4fb3652db0ff3687e073b5a68990174f87a1746e
Anh031006 <dangngocminhanh76@gmail.com>
2025-10-15 15:37:15 +0000

    fix

--- js/initialization.js
+++ js/initialization.js
@@ -4,7 +4,7 @@ function createProduct() {
             id: 1,
             status:1,
             title:'Bột Chiên',
-            img:'access/image /Bột Chiên.JPG',
+            img:'access/image /product/Bột Chiên.JPG',
             category: 'Món ăn vặt',
             price: 40000,
             desc:'Bột chiên giòn rụm, ăn cùng với trứng và đu đủ bào, chấm cùng nước tuong pha chua ngọt. Món ăn vặt thơm ngon đặc trứng của Sài Gòn',
@@ -14,7 +14,7 @@ function createProduct() {
             id: 2,
             status:'1',
             title:'Bún Đậu Mắm Tôm',
-            img:'access/image /Bún Đậu.jpg',
+            img:'access/image /product/Bún Đậu.jpg',
             category: 'Món mặn',
             price: 120000,
             desc:'Bún đậu mắm tôm thêm ngon, gồm bún lá, đậu hũ chiên vàng, thịt luộc, chả cốm và dưa leo. Ăn kèm mắm tôm pha chua ngọt, cay đậm vị Việt ',
@@ -24,7 +24,7 @@ function createProduct() {
             id: 3,
             status:'1',
             title:'Chè Thái',
-            img:'access/image /Chè Thái.JPG',
+            img:'access/image /product/Chè Thái.JPG',
             category: 'Món tráng miệng',
             price: 35000,
             desc:'Chè Thái mát lạnh lạnh với nhiều loại trái cây tươi, thạch giòn, mít thơm, nhãn mọng và sầu riêng béo ngậy. Hòa quyện cùng nước cốt dừa ngọt dịu, giải nhiệt tuyệt vời ',
@@ -34,7 +34,7 @@ function createProduct() {
             id: 4,
             status:'1',
             title:'Milo Dầm',
-            img:'access/image /Milo Dầm.PNG',
+            img:'access/image /product/Milo Dầm.PNG',
             category: 'Nước uống',
             price: 30000,
             desc:'',
@@ -44,7 +44,7 @@ function createProduct() {
             id: '############',
             status:'#########',
             title:'#################',
-            img:'access/image /Gỏi Cuốn.jpg',
+            img:'access/image /product/Gỏi Cuốn.jpg',
             category: 'Món mặn',
             price: '################',
             desc:'########################################################################',
@@ -54,7 +54,7 @@ function createProduct() {
             id: '############',
             status:'#########',
             title:'#################',
-            img:'access/image /Phở.jpg',
+            img:'access/image /product/Phở.jpg',
             category: 'Món mặn',
             price: '################',
             desc:'########################################################################',
@@ -64,16 +64,44 @@ function createProduct() {
             id: '############',
             status:'#########',
             title:'#################',
-            img:'access/image /Gà Luộc.jpg',
+            img:'access/image /product/Gà Luộc.jpg',
             category: 'Món mặn',
             price: '################',
             desc:'########################################################################',
       
         },
-        
+
         ]
     }
 }
 function createAdminAccount() {
-
+    let accounts = localStorage.getItem("accounts");
+    if(!accounts){
+        accounts = [];
+        accounts.push({
+            fullname:"Lê Thị Ngọc Thảo",
+            phone:"NgocThao0412",
+            password: "123456@",
+            address: 'https://github.com/NgocThao0412',
+            email: 'happyhappy123@gmail.com',
+            status: 1,
+            join: new Date(),
+            cart:[],
+            usertype: 1
+    })
+    accounts.push({
+        fullname:"Đặng Ngọc Minh Anh",
+        phone:"1234567890",
+        address:'',
+        status: 1,
+        join: new Date(),
+        cart:[],
+        userType:1
+    })
+    localStorage.setItem('accounts', JSON.stringify(accounts));
+    }
+    Window.onload = function(){
+        createProduct();
+        createAdminAccount();
+    }
 }
